9e58dee84926e1c1c511acfa270f2164017564a8
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-06-18 18:55:03 +0200

    add r2b project

---
+++ r2b/r2b.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		r2b.ino
+ Created:	6/18/2021 6:51:53 PM
+ Author:	burdo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
