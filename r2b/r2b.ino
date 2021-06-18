2b851a3bd1ba73d076ad1f9a919dcf06e23801d7
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-06-18 19:06:03 +0200

    move stuff around

--- r2b/r2b.ino
+++ r2b/r2b.ino
@@ -1,15 +1,44 @@
-/*
- Name:		r2b.ino
- Created:	6/18/2021 6:51:53 PM
- Author:	burdo
-*/
+//LEDs an den digitalen PINs 10,11,12,13
+const byte LED[] = { 13,12,11,10 };
+int state = 1;
+long last = 0;
 
-// the setup function runs once when you press reset or power the board
+#define Btn1 A1 //Taster S1
+#define Btn2 A2 //Taster S2
+#define Btn3 A3 //Taster S3
+
+#define ON LOW
+#define OFF HIGH
+
 void setup() {
-
+    Serial.begin(9600);
+    pinMode(LED[0], OUTPUT); //LED1 am digitalen PIN 13
+    pinMode(LED[1], OUTPUT); //LED2 am digitalen PIN 12
+    pinMode(LED[2], OUTPUT); //LED3 am digitalen PIN 11
+    pinMode(LED[3], OUTPUT); //LED4 am digitalen PIN 10
+    setLEDs(0);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+    if (millis() - last < analogRead(0) * 3) {
+        return;
+    }
+
+    if (setLEDs(state)) {
+        state = state << 1;
+        last = millis();
+    }
+    else {
+        state = 1;
+    }
+}
+
+bool setLEDs(int status) {
+    Serial.print("setting status: 0b");
+    Serial.println(status, BIN);
+    digitalWrite(LED[0], (status & 0b1000) != 0 ? ON : OFF);
+    digitalWrite(LED[1], (status & 0b0100) != 0 ? ON : OFF);
+    digitalWrite(LED[2], (status & 0b0010) != 0 ? ON : OFF);
+    digitalWrite(LED[3], (status & 0b0001) != 0 ? ON : OFF);
+    return status < 9;
 }
